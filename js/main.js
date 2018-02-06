dc28f45454f3b9cd99e345948fc96f9d5b33dee7
magia-sloon <35709991+magia-sloon@users.noreply.github.com>
2018-02-06 23:40:14 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
-/*FACE*/
-$(document).ready(function(){
-    var tab = $('.tab');
-    tab.click(function(){
-       var target = $(this).attr('href');
-        $('.acord-win').slideUp();
-        $(target).slideDown();
-    });
-});
-/*BODY SHUGARING*/
-$(document).ready(function(){
-    var tab_body = $('.tab-body');
-    tab_body.click(function(){
-       var target = $(this).attr('href');
-        $('.acord-win-body').slideUp();
-        $(target).slideDown();
-    });
-});
-/*BODY WRAPPING*/
-$(document).ready(function(){
-    var tab_body_wrapping = $('.tab-body-wrapping');
-    tab_body_wrapping.click(function(){
-       var target = $(this).attr('href');
-        $('.acord-win-body-wrapping').slideUp();
-        $(target).slideDown();
-    });
-});
-
  $(document).ready(function(){
     /*$("#menu").on("click","a", function (event)*/
    $(".scrolling").on("click", function (event)
@@ -33,7 +5,7 @@ $(document).ready(function(){
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 3000);
+        $('body,html').animate({scrollTop: top}, 1800);
     });
 });
 
